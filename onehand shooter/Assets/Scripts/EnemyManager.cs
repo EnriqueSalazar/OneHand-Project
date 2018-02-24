baad0e3ecef2cc2d26fa5b2b63e70d4359ca4eb6
Enrique Salazar <enrique.salazar@gmail.com>
2018-02-24 11:42:50 +0100

    fix

---
+++ onehand shooter/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Spawn()
+    {
+        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), Camera.main.farClipPlane / 2));
+        Instantiate(enemy, screenPosition, Quaternion.identity);
+    }
+}
